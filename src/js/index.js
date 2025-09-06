bd3a028c56d798d4a260d9c2596b5e27d9aa426e
anchen2 <anchen2@illinois.edu>
2025-09-06 16:36:47 -0500

    CS409 MP0 finished webpage: anchen2

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
 /* Your JS here. */
 console.log('Hello World!')
+
+const button = document.getElementById('button');
+
+const colors = ['white','red', 'blue', 'green', 'yellow', 'purple', 'orange'];
+let i = 0;
+
+button.addEventListener('click', function() {
+    document.body.style.backgroundColor = colors[i];
+    i++;
+
+    if (i >= colors.length) {
+        i = 0;
+    }
+});
